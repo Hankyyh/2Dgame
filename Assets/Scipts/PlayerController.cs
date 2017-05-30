dd1e232b661e2a9b22a285153c48b4e38a3e6345
Hankyyh <yunhany@hotmail.com>
2017-05-29 22:29:08 -0700

    2.1

---
+++ Assets/Scipts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+	public float speed;
+
+	private Rigidbody2D rb2d;
+	void Start() {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+	}
+}
